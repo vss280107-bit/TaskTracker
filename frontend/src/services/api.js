654f3be610e79556f9c5100befc3d484287ed885
Vanshika Saxena <vss280107@gmail.com>
2026-06-29 19:27:28 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5001/api";
+const BASE_URL = "https://tasktracker-53za.onrender.com/api";
 
 console.log("BASE_URL:", BASE_URL);
 
